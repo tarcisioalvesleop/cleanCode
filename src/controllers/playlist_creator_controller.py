ff22e93a2e912f8e9a117ddaa57b965d5d89189c
tarcisioalvesleop <tarcisioalvesleop@hotmail.com>
2026-01-30 11:02:53 -0300

    feat: Implementing playlist_creator_controller

---
+++ src/controllers/playlist_creator_controller.py
@@ -0,0 +1,35 @@
+import random
+from src.models.repositories.musics_repository import musics_repository
+
+class PlaylistCreatorController:
+    def create_playlist(self) -> dict:
+        # Principio da Responsabilidade Única
+        try:
+            musics = self.__get_all_musics_and_verify()
+            playlist = self.__create_playlist(musics)
+            return self.__format_response(playlist)
+        except Exception as exception:
+            self.__format_error_response(exception)
+
+    def __get_all_musics_and_verify(self) -> list:
+        musics = musics_repository.get_all_songs()
+        if musics is []:
+            raise Exception["Nenhuma musica registrada!"]
+        
+        return musics
+    
+    def __create_playlist(self, musics: list) -> list:
+        random.shuffle(musics)
+        return musics
+    
+    def __format_response(self, playlist: list) -> dict:
+        return {
+            "success": True,
+            "playlist": playlist
+        }
+    
+    def __format_error_response(self, err:Exception) -> dict:
+        return {
+            "success": False,
+            "error": str(err) 
+        }
